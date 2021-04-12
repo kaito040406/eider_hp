c5e58efcbec3c1f432aa20a57adb31cf56ebe59a
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-04-12 21:36:08 +0900

    404ページの追加

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div class="pastrecords clear">
+    <div class="past-title scrollanime slide-left">
+        <!-- <h3 class="main-course-item">PAST RECORD</h3> -->
+        <h3 class="main-course-item">404</h3>
+        <p class="main-course-item-2">お探しのページを見つけることが出来ませんでした。</p>
+    </div>
+</div>
+<?php get_footer(); ?>
