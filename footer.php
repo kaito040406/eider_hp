8f1e78a9003620b55ca2338898b0e8517dd3bf11
ookuraseiya <79490150+ookuraseiya@users.noreply.github.com>
2021-04-21 15:49:26 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             <div class="gaiyou ">
                 <img src="https://fly3.gigafile.nu/folder-pc-thumb.php?file=B%2F0326-29944128eaa52f758438eab2aa9e7e7f%2FK6ZtRhhH.png&size=300" alt="">
                 <p>〒661-0026 兵庫県尼崎市水堂町３－１－２１ 中塚ビル３０２号</p>
-                <p>TEL:06-6439-7639 受付時間：9:00～18:00（平日のみ)</p>
+                <p>TEL:0120-930540 受付時間：9:00～18:00（平日のみ)</p>
             </div>
             <div class="foom">
                 <div class="foom1 ">
@@ -20,9 +20,9 @@
                     </a>
                 </div>
                 <div class="foom2">
-                    <a href="tell:0000-00-0000">
+                    <a href="tell:0120-947771">
                         <div class="foom2-me">
-                            <h3>tell:06-6439-7639</h3>
+                            <h3>tell:0120-947771</h3>
                             <p>受付時間：9:00～18:00（平日のみ)</p>
                         </div>
                     </a>
